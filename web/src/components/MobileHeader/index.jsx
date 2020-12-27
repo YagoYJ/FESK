3ccd264be3bf5f273f0c70838a795e51a9fcc229
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-12-27 00:06:30 -0300

    Show and hide the mobile menu

--- web/src/components/MobileHeader/index.jsx
+++ web/src/components/MobileHeader/index.jsx
@@ -1,10 +1,23 @@
 import logo from "../../assets/logo.png";
 import iconUserLogin from "../../assets/icon-user-login.png";
 import iconMenuBars from "../../assets/icon-menu-bars.png";
+import iconClose from "../../assets/icon-close.png";
 
 import "./styles.css";
+import { useState } from "react";
 
 export default function MobileHeader() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  function handleOpenMenu() {
+    setMenuVisible(true);
+    console.log(menuVisible);
+  }
+
+  function handleCloseMenu() {
+    setMenuVisible(false);
+  }
+
   return (
     <div className="header mobileHeader">
       <div className="options">
@@ -13,11 +26,26 @@ export default function MobileHeader() {
           <button className="iconButton">
             <img src={iconUserLogin} alt="Login de usuário" />
           </button>
-          <button className="iconButton">
+          <button className="iconButton" onClick={handleOpenMenu}>
             <img src={iconMenuBars} alt="Barras de menu" />
           </button>
         </div>
       </div>
+      {menuVisible && (
+        <nav className="menu">
+          <button className="iconButton closeButton" onClick={handleCloseMenu}>
+            <img src={iconClose} alt="Botão para fechar o menu" />
+          </button>
+          <ul>
+            <li>Início</li>
+            <li>Institucional</li>
+            <li>Eventos</li>
+            <li>Galeria</li>
+            <li>Filiadas</li>
+            <li>Ranking</li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }

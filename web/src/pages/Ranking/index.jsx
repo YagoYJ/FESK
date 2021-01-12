d47d8dcd7bb82ad52f24946a06cf2075cdd491fb
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2021-01-11 23:05:43 -0300

    initialized the ranking page

--- web/src/pages/Ranking/index.jsx
+++ web/src/pages/Ranking/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header";
+import Card from "../../components/Card";
 import Footer from "../../components/Footer";
 
 import "./styles.css";
@@ -7,7 +8,18 @@ export default function Ranking() {
   return (
     <div className="rakingPage">
       <Header />
-      <main></main>
+      <main>
+        <h1>Rankings</h1>
+        <Card cardStyle="gray">
+          <div className="container">
+            <ul>
+              <li>Campeonato 1</li>
+              <li>Campeonato 2</li>
+              <li>Campeonato 3</li>
+            </ul>
+          </div>
+        </Card>
+      </main>
       <Footer />
     </div>
   );
